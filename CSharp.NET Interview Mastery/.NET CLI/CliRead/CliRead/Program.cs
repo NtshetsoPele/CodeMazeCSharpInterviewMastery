bc325c2b857532c28ee7080e52774a4c21f52010
NtshetsoPele <mothobi.bald@gmail.com>
2026-03-05 20:54:22 +0200

    first commit

---
+++ CSharp.NET Interview Mastery/.NET CLI/CliRead/CliRead/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json;
+
+var json = await File.ReadAllTextAsync("C:/ProgramData/CodeMaze/data.json");
+using var doc = JsonDocument.Parse(json);
+var root = doc.RootElement;
+var peeps = root.GetProperty("people").EnumerateArray().Select(p => new Person
+{
+    Name = p.GetProperty("name").GetString(),
+    Age = p.GetProperty("age").GetInt32(),
+    City = p.GetProperty("city").GetString()
+}).ToList();
+Console.WriteLine(peeps[0].Name);
+
+return;
+
+class Person
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public string City{ get; set; }
+}
---
+++ CSharp.NET Interview Mastery/.NET CLI/hello-cli/Hello/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Hello;
+
+Console.WriteLine("Hello, World!");
+
+namespace Hello
+{
+    public class Example
+    {
+        public void Test()
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+}
